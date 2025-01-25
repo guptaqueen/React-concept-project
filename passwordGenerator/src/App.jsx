42d49a06744f604a156fec790e8853aa1b0f619b
Manisha Gupta <rouniyarmanisha@gmail.com>
2025-01-25 13:57:48 +0545

    main

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -34,13 +34,13 @@ const copyPasswordToClipboard = useCallback(() =>{passwordRef.current?.select();
   return (
     <>
       <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-5  my-8 text-orange-500 bg-gray-800">
-        <h1 className=" text-center text-white my-3">Password Generator</h1>
+        <h1 className=" text-center text-white my-3 ">Password Generator</h1>
 
         <div className="flex shadow rounded-lg overflow-hidden mb-4">
           <input
             type="text"
             value={password}
-            className="outline-none w-full py-1 px-3"
+            className="outline-none w-full py-1 px-3 "
             placeholder="Password"
             readOnly
             ref={passwordRef}
